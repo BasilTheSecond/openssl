79e48a91c45f4c64dd88f82c67a6460c4987c019
dostojic <dragan_ostojic@bio-rad.com>
2015-07-23 13:00:59 -0400

    Final version

--- examples/gcm/gcm.c
+++ examples/gcm/gcm.c
@@ -15,12 +15,8 @@ static unsigned char decm[1024];
 static unsigned char aad[1024]; // Additional associated data (can be any size)
 static unsigned char tag[TAG_SIZE];
 
-
-static int
-encrypt();
-
-static int
-decrypt();
+static int encrypt();
+static int decrypt();
 
 int 
 main(	int argc, 
@@ -37,73 +33,8 @@ main(	int argc,
 	// Check that the authentication works
 	//encm[0] ^= 0xff; // Tamper with the ciphertext
 	//aad[0] ^= 0xff; // Tamper with the additional (plain-text) data
-
-	//DECRYPTION PART
-	//Now Decryption of the data.
-	//Then decrypt the data.
-
 	
-	EVP_CIPHER_CTX *ctx     = EVP_CIPHER_CTX_new();
-	
-	assert(ctx != NULL);
-	
-	//Get the cipher.
-	//const EVP_CIPHER *cipher  = EVP_aes_128_gcm ();
-	const EVP_CIPHER *cipher  = EVP_aes_256_gcm ();
-	
-	//Set just cipher.
-	retv    = EVP_DecryptInit_ex(ctx, cipher, NULL, NULL, NULL);
-
-	if (retv != 1) {
-		printf("Error\n");
-		goto __exit;
-	}
-
-	//Set Nonce size.
-	retv    = EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, sizeof(ivbuf), NULL);
-
-	if (retv != 1) {
-		printf("Error\n");
-		goto __exit;
-	}
-	
-	//Set key and IV (nonce).
-	retv    = EVP_DecryptInit_ex(ctx, NULL, NULL, (const unsigned char*)keybuf, (const unsigned char *)ivbuf);
-
-	if (retv != 1) {
-		printf("Error\n");
-		goto __exit;
-	}
-
-	//Set authentication tag
-	retv    = EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, TAG_SIZE, (unsigned char *)tag);
-
-	if (retv != 1) {
-		printf("Error\n");
-		goto __exit;
-	}
-	
-	//[Optional for GCM]
-	//Add Additional associated data (AAD).
-	// This can be called zero or more times as required
-	retv    = EVP_DecryptUpdate(ctx, NULL, (int *)&declen, (const unsigned char *)aad, sizeof(aad));
-
-	if (retv != 1) {
-		printf("Error\n");
-		goto __exit;
-	}
-
-	//Decrypt the data.
-	// EVP_DecryptUpdate can be called multiple times if necessary
-	retv    = EVP_DecryptUpdate(ctx, decm, (int *)&declen, (const unsigned char *)encm, enclen);
-
-	if (retv != 1) {
-		printf("Error\n");
-		goto __exit;
-	}
-	
-	//Finalize.
-	retv    = EVP_DecryptFinal_ex(ctx, (unsigned char*)decm + declen, (int *)&declen2);
+	retv = decrypt();
 
 	// Check that ciphertext or AAD hasn't been tampered with
 	if (retv == 1) {
@@ -112,11 +43,8 @@ main(	int argc,
 	else {
 		printf("Fail\n");
 	}
-
+	
 __exit:
-
-  /* Clean up */  
-  EVP_CIPHER_CTX_free(ctx);
 
   return retv == 1 ? 0 : 1;
 }
@@ -220,6 +148,78 @@ __exit:
 
 static int
 decrypt() {
-	return 1;
+	//DECRYPTION PART
+	//Now Decryption of the data.
+	//Then decrypt the data.
+
+	
+	EVP_CIPHER_CTX *ctx     = EVP_CIPHER_CTX_new();
+	
+	assert(ctx != NULL);
+	
+	//Get the cipher.
+	//const EVP_CIPHER *cipher  = EVP_aes_128_gcm ();
+	const EVP_CIPHER *cipher  = EVP_aes_256_gcm ();
+	
+	//Set just cipher.
+	int retv    = EVP_DecryptInit_ex(ctx, cipher, NULL, NULL, NULL);
+
+	if (retv != 1) {
+		printf("Error\n");
+		goto __exit;
+	}
+
+	//Set Nonce size.
+	retv    = EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, sizeof(ivbuf), NULL);
+
+	if (retv != 1) {
+		printf("Error\n");
+		goto __exit;
+	}
+	
+	//Set key and IV (nonce).
+	retv    = EVP_DecryptInit_ex(ctx, NULL, NULL, (const unsigned char*)keybuf, (const unsigned char *)ivbuf);
+
+	if (retv != 1) {
+		printf("Error\n");
+		goto __exit;
+	}
+
+	//Set authentication tag
+	retv    = EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, TAG_SIZE, (unsigned char *)tag);
+
+	if (retv != 1) {
+		printf("Error\n");
+		goto __exit;
+	}
+	
+	//[Optional for GCM]
+	//Add Additional associated data (AAD).
+	// This can be called zero or more times as required
+	retv    = EVP_DecryptUpdate(ctx, NULL, (int *)&declen, (const unsigned char *)aad, sizeof(aad));
+
+	if (retv != 1) {
+		printf("Error\n");
+		goto __exit;
+	}
+
+	//Decrypt the data.
+	// EVP_DecryptUpdate can be called multiple times if necessary
+	retv    = EVP_DecryptUpdate(ctx, decm, (int *)&declen, (const unsigned char *)encm, enclen);
+
+	if (retv != 1) {
+		printf("Error\n");
+		goto __exit;
+	}
+	
+	//Finalize.
+	retv    = EVP_DecryptFinal_ex(ctx, (unsigned char*)decm + declen, (int *)&declen2);
+
+__exit:
+
+	/* Clean up */
+	EVP_CIPHER_CTX_free(ctx);
+	
+	return retv;
 }
 
